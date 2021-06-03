c0291ebd37265198562ec69e30790083deeb06a1
chenhongchen <412130100@qq.com>
2021-06-03 08:48:52 +0800

    初始化项目

---
+++ ios/Classes/CacheFadeImagePlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface CacheFadeImagePlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/CacheFadeImagePlugin.m
@@ -0,0 +1,20 @@
+#import "CacheFadeImagePlugin.h"
+
+@implementation CacheFadeImagePlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"cache_fade_image"
+            binaryMessenger:[registrar messenger]];
+  CacheFadeImagePlugin* instance = [[CacheFadeImagePlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
