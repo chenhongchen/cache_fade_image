6c2b23a4541847d549879240f3ce14f46bcdf129
chenhongchen <412130100@qq.com>
2021-07-16 11:23:55 +0800

    完善代码

--- lib/cache_fade_image.dart
+++ lib/cache_fade_image.dart
@@ -197,7 +197,7 @@ class CacheFadeImageState extends State<CacheFadeImage>
             _brightness == Brightness.dark)
         ? widget.darkPlaceholder
         : widget.placeholder;
-    if (widget._src.length <= 0) {
+    if (widget._src.length <= 0 || widget._src.indexOf('http') != 0) {
       return _buildPlaceholderWidget();
     }
     return ExtendedImage.network(
